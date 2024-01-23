fadad04df4257532a9f48e1dbc07ba0c91e9d3b0
dylan <DMathewToh1999@gmail.com>
2024-01-23 10:45:59 -0500

    added product list in app.js

---
+++ Lab2/app.js
@@ -0,0 +1,101 @@
+var products = [
+    {
+      name: "milk",
+      vegetarian: true,
+      vegan: false,
+      lactoseIntolerant: false,
+      glutenIntolerant: true,
+      organic: false,
+      price: 5.50
+    },
+    {
+      name: "bread",
+      vegetarian: true,
+      vegan: true,
+      lactoseIntolerant: true,
+      glutenIntolerant: false,
+      organic: false,
+      price: 4
+    },
+    {
+      name: "organic egg",
+      vegetarian: true,
+      vegan: false,
+      lactoseIntolerant: true,
+      glutenIntolerant: true,
+      organic: true,
+      price: 5.50
+    },
+    {
+      name: "organic carrots",
+      vegetarian: true,
+      vegan: true,
+      lactoseIntolerant: true,
+      glutenIntolerant: true,
+      organic: true,
+      price: 4.20
+    },
+    {
+      name: "apples",
+      vegetarian: true,
+      vegan: true,
+      lactoseIntolerant: true,
+      glutenIntolerant: true,
+      organic: false,
+      price: 2.90
+    },
+    {
+      name: "honey",
+      vegetarian: true,
+      vegan: false,
+      lactoseIntolerant: true,
+      glutenIntolerant: true,
+      organic: false,
+      price: 6.50
+    },
+    {
+      name: "cornflake",
+      vegetarian: true,
+      vegan: true,
+      lactoseIntolerant: true,
+      glutenIntolerant: false,
+      organic: false,
+      price: 7
+    },
+    {
+      name: "organic sunflower oil",
+      vegetarian: true,
+      vegan: true,
+      lactoseIntolerant: true,
+      glutenIntolerant: true,
+      organic: true,
+      price: 10.50
+    },
+    {
+      name: "pasta",
+      vegetarian: true,
+      vegan: true,
+      lactoseIntolerant: true,
+      glutenIntolerant: false,
+      organic: false,
+      price: 7.50
+    },
+    {
+      name: "organic chicken thigh",
+      vegetarian: false,
+      vegan: false,
+      lactoseIntolerant: true,
+      glutenIntolerant: true,
+      organic: true,
+      price: 15.50
+    },
+    {
+      name: "freshly-caught organic salmon",
+      vegetarian: false,
+      vegan: false,
+      lactoseIntolerant: true,
+      glutenIntolerant: true,
+      organic: true,
+      price: 18
+    },
+];
