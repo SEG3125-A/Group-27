9e6d067d945a3f57693f6ad8d58b1c791763fffa
D-Mathew <DMathewToh1999@gmail.com>
2024-02-10 01:01:54 -0500

    set up for lab 5

---
+++ Lab5/main.js
@@ -0,0 +1,24 @@
+function populateTimeSlots(start, end, interval) {
+    const select = document.getElementById('appointment');
+    var currentTime = start;
+
+    while (currentTime < end) {
+        const option = document.createElement('option');
+        const timeString = new Date(currentTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+        option.value = timeString;
+        option.textContent = timeString;
+        console.log(option)
+        select.appendChild(option);
+
+        // Increment the current time by the interval
+        currentTime.setMinutes(currentTime.getMinutes() + interval);
+    }
+}
+
+const startTime = new Date();
+startTime.setHours(12, 0, 0);
+
+const endTime = new Date();
+endTime.setHours(18, 0, 0);
+
+populateTimeSlots(startTime, endTime, 15);
